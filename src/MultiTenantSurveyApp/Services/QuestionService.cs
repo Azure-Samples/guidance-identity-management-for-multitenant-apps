95f52aaeeec417f47b007166597745f9b4630328
Francis Cheung <fcheung@microsoft.com>
2015-11-30 13:21:41 -0800

    Refactored Question and Survey Service to use HttpClient Extension method.

--- src/MultiTenantSurveyApp/Services/QuestionService.cs
+++ src/MultiTenantSurveyApp/Services/QuestionService.cs
@@ -4,9 +4,11 @@
 using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Http;
 using Microsoft.Extensions.OptionsModel;
+using MultiTenantSurveyApp.Common;
 using MultiTenantSurveyApp.Configuration;
 using MultiTenantSurveyApp.DAL.DTOs;
 using MultiTenantSurveyApp.Models;
@@ -29,56 +31,51 @@ namespace MultiTenantSurveyApp.Services
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly IAccessTokenService _accessTokenService;
         private readonly HttpClient _httpClient;
-
+        private readonly CancellationToken _cancellationToken;
+        
         public QuestionService(HttpClientService factory, IHttpContextAccessor httpContextAccessor, IAccessTokenService accessTokenService)
         {
             _httpContextAccessor = httpContextAccessor;
             _httpClient = factory.GetHttpClient();
             _accessTokenService = accessTokenService;
+            _cancellationToken = httpContextAccessor?.HttpContext?.RequestAborted ?? CancellationToken.None;
         }
 
         public async Task<ApiResult<QuestionDTO>> GetQuestionAsync(int id)
         {
             var path = $"/questions/{id}";
             var requestMessage = new HttpRequestMessage(HttpMethod.Get, path);
-            var response = await SendMessagewithBearerTokenAsync(requestMessage).ConfigureAwait(false);
+            var response = await _httpClient.SendRequestWithBearerTokenAsync(HttpMethod.Get, path, null,
+                        await _accessTokenService.GetTokenForWebApiAsync(_httpContextAccessor.HttpContext.User)
+                                .ConfigureAwait(false), _cancellationToken);
             return await ApiResult<QuestionDTO>.FromResponseAsync(response).ConfigureAwait(false);
         }
 
         public async Task<ApiResult<QuestionDTO>> CreateQuestionAsync(QuestionDTO question)
         {
             var path = $"/surveys/{question.SurveyId}/questions";
-            var requestMessage = new HttpRequestMessage(HttpMethod.Post, path) {Content = CreateJsonContent(question)};
-            var response = await SendMessagewithBearerTokenAsync(requestMessage).ConfigureAwait(false);
+            var response = await _httpClient.SendRequestWithBearerTokenAsync(HttpMethod.Post, path, question,
+                        await _accessTokenService.GetTokenForWebApiAsync(_httpContextAccessor.HttpContext.User)
+                                .ConfigureAwait(false), _cancellationToken);
             return await ApiResult<QuestionDTO>.FromResponseAsync(response).ConfigureAwait(false);
         }
 
         public async Task<ApiResult<QuestionDTO>> UpdateQuestionAsync(QuestionDTO question)
         {
             var path = $"/questions/{question.Id}";
-            var requestMessage = new HttpRequestMessage(HttpMethod.Put, path) {Content = CreateJsonContent(question)};
-            var response = await SendMessagewithBearerTokenAsync(requestMessage).ConfigureAwait(false);
+            var response = await _httpClient.SendRequestWithBearerTokenAsync(HttpMethod.Put, path, question,
+                        await _accessTokenService.GetTokenForWebApiAsync(_httpContextAccessor.HttpContext.User)
+                                .ConfigureAwait(false), _cancellationToken);
             return await ApiResult<QuestionDTO>.FromResponseAsync(response).ConfigureAwait(false);
         }
 
         public async Task<ApiResult> DeleteQuestionAsync(int id)
         {
             var path = $"/questions/{id}";
-            var requestMessage = new HttpRequestMessage(HttpMethod.Delete, path);
-            var response = await SendMessagewithBearerTokenAsync(requestMessage).ConfigureAwait(false);
+            var response = await _httpClient.SendRequestWithBearerTokenAsync(HttpMethod.Delete, path, null,
+                        await _accessTokenService.GetTokenForWebApiAsync(_httpContextAccessor.HttpContext.User)
+                                .ConfigureAwait(false), _cancellationToken);
             return new ApiResult { Response = response };
-        }
-
-        private static HttpContent CreateJsonContent<T>(T item)
-        {
-            var json = JsonConvert.SerializeObject(item);
-            return new StringContent(json, System.Text.Encoding.UTF8, "application/json");
-        }
-
-        private async Task<HttpResponseMessage> SendMessagewithBearerTokenAsync(HttpRequestMessage requestMessage)
-        {
-            requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", await _accessTokenService.GetTokenForWebApiAsync(_httpContextAccessor.HttpContext.User).ConfigureAwait(false));
-            return await _httpClient.SendAsync(requestMessage).ConfigureAwait(false);
         }
     }
 }
