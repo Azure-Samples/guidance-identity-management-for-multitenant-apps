3cff2c9ea0f7ce8e164a5d50483d710b4c8546e4
Andrew Oakley <anoakley@microsoft.com>
2015-11-30 21:56:19 -0800

    Restructed ADAL credentials and added comments.

--- src/MultiTenantSurveyApp.TokenStorage/RedisTokenCache.cs
+++ src/MultiTenantSurveyApp.TokenStorage/RedisTokenCache.cs
@@ -9,41 +9,44 @@ using StackExchange.Redis;
 
 namespace MultiTenantSurveyApp.TokenStorage
 {
-    // sample implementation of a token cache which persists tokens specific to a user to redis to be used in multi tenanted scenarios
-    // the key is the users object unique object identifier   
-    // [masimms-roshar] Since we expect this to act as a copy-paste sample, please add an optional verbose log for 
-    // all access to ease future debugging
-    //[manikris] done
+    /// <summary>
+    /// Sample implementation of a token cache which persists tokens specific to a user to Redis to be used in multi-tenanted scenarios
+    /// the key is the users object unique object identifier.
+    /// </summary>
     public class RedisTokenCache : TokenCache
     {
         private IDatabase _cache;
         private TokenCacheKey _key;
         private ILogger _logger;
 
+        /// <summary>
+        /// Factory method to create instances of <see cref="MultiTenantSurveyApp.TokenStorage.RedisTokenCache"/>.  The constructor
+        /// is intentionally made private.  Since we need to do some async initialization, the instance of the class is not ready
+        /// for use at the completion of the constructor.
+        /// </summary>
+        /// <param name="connection">An instance of <see cref="StackExchange.Redis.IConnectionMultiplexer"/> to use for a connection to Redis.</param>
+        /// <param name="key">An instance of <see cref="MultiTenantSurveyApp.TokenStorage.TokenCacheKey"/> containing the key for the new cache.</param>
+        /// <param name="logger">An instance of <see cref="Microsoft.Extensions.Logging.ILogger"/> to use for logging.</param>
+        /// <returns>An initialized instance of <see cref="MultiTenantSurveyApp.TokenStorage.TokenCacheKey"/>.</returns>
         public async static Task<RedisTokenCache> CreateCacheAsync(IConnectionMultiplexer connection, TokenCacheKey key, ILogger logger)
         {
-            var cache = connection.GetDatabase();
-            byte[] cachedData = null;
-            try
-            {
-                cachedData = await cache.StringGetAsync(key.ToString()).ConfigureAwait(false);
-                logger.TokensRetrievedFromStore(key.ToString());
-            }
-            catch (Exception exp)
-            {
-                logger.ReadFromCacheFailed(exp);
-                throw;
-            }
-
-            return new RedisTokenCache(cache, key, logger, cachedData);
+            var cache = new RedisTokenCache(connection, key, logger);
+            await cache.InitializeAsync();
+            return cache;
         }
 
-        private RedisTokenCache(IDatabase database, TokenCacheKey key, ILogger logger, byte[] cachedData)
+        /// <summary>
+        /// Initializes a new instance of <see cref="MultiTenantSurveyApp.TokenStorage.RedisTokenCache"/>.
+        /// </summary>
+        /// <param name="connection">An instance of <see cref="StackExchange.Redis.IConnectionMultiplexer"/> to use for a connection to Redis.</param>
+        /// <param name="key">An instance of <see cref="MultiTenantSurveyApp.TokenStorage.TokenCacheKey"/> containing the key for this instance of cache.</param>
+        /// <param name="logger">An instance of <see cref="Microsoft.Extensions.Logging.ILogger"/> to use for logging.</param>
+        private RedisTokenCache(IConnectionMultiplexer connection, TokenCacheKey key, ILogger logger)
             : base()
         {
-            if (database == null)
+            if (connection == null)
             {
-                throw new ArgumentNullException(nameof(database));
+                throw new ArgumentNullException(nameof(connection));
             }
 
             if (key == null)
@@ -56,21 +59,44 @@ namespace MultiTenantSurveyApp.TokenStorage
                 throw new ArgumentNullException(nameof(logger));
             }
 
-            _cache = database;
+            _cache = connection.GetDatabase();
             _key = key;
             _logger = logger;
             AfterAccess = AfterAccessNotification;
-            Deserialize(cachedData);
         }
 
-        // Triggered right after ADAL accessed the cache.
+        /// <summary>
+        /// Initializes the cache data from Redis.  This is a separate method since we cannot have async methods in a constructor.
+        /// </summary>
+        /// <returns></returns>
+        private async Task InitializeAsync()
+        {
+            try
+            {
+                byte[] cachedData = await _cache.StringGetAsync(_key.ToString())
+                    .ConfigureAwait(false);
+                this.Deserialize(cachedData);
+                _logger.TokensRetrievedFromStore(_key.ToString());
+            }
+            catch (Exception exp)
+            {
+                _logger.ReadFromCacheFailed(exp);
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Handles the AfterAccessNotification event, which is triggered right after ADAL accesses the cache.
+        /// </summary>
+        /// <param name="args">An instance of <see cref="Microsoft.IdentityModel.Clients.ActiveDirectory.TokenCacheNotificationArgs"/> containing information for this event.</param>
         public async void AfterAccessNotification(TokenCacheNotificationArgs args)
         {
             if (this.HasStateChanged)
             {
                 try
                 {
-                    await _cache.StringSetAsync(this._key.ToString(), this.Serialize()).ConfigureAwait(false);
+                    await _cache.StringSetAsync(this._key.ToString(), this.Serialize())
+                        .ConfigureAwait(false);
                     _logger.TokensWrittenToStore(args.ClientId, args.UniqueId, args.Resource);
                     this.HasStateChanged = false;
                 }
@@ -82,7 +108,9 @@ namespace MultiTenantSurveyApp.TokenStorage
             }
         }
 
-        // clean the cache of all tokens associated with the user.
+        /// <summary>
+        /// Clears the cache of all tokens for the user.
+        /// </summary>
         public override void Clear()
         {
             base.Clear();
---
+++ src/MultiTenantSurveyApp/Security/ClientCredentialService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.OptionsModel;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using MultiTenantSurveyApp.Common.Configuration;
+using MultiTenantSurveyApp.Configuration;
+
+namespace MultiTenantSurveyApp.Security
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ClientCredentialService : ICredentialService
+    {
+        private AdalCredential _credential;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="options"></param>
+        public ClientCredentialService(IOptions<ConfigurationOptions> options)
+        {
+            var config = options?.Value?.AzureAd;
+            _credential = new AdalCredential(new ClientCredential(config.ClientId, config.ClientSecret));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public Task<AdalCredential> GetCredentialsAsync()
+        {
+            return Task.FromResult(_credential);
+        }
+    }
+}
