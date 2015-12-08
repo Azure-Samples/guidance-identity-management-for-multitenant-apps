ad915acd45b8fd4413790508edd6304c49138da1
Andrew Oakley <anoakley@microsoft.com>
2015-12-08 15:11:09 -0800

    Combined PowerShell scripts

--- scripts/Setup-KeyVault.ps1
+++ scripts/Setup-KeyVault.ps1
@@ -1,9 +1,34 @@
 ﻿param(
-[parameter(Mandatory=$true, ParameterSetName="SetupKeyVault")][string]$ResourceGroupName,
-[parameter(Mandatory=$true, ParameterSetName="SetupKeyVault")][string]$Location,
-[parameter(Mandatory=$true, ParameterSetName="SetupKeyVault")][string]$ApplicationId,
-[parameter(Mandatory=$true)][string]$KeyVaultName,
-[parameter(Mandatory=$false, ParameterSetName="SetValues")][PSObject[]]$Values
+[parameter(Mandatory=$true, ParameterSetName="GenerateCertificate")]
+[string]$Subject,
+
+[parameter(Mandatory=$false, ParameterSetName="GenerateCertificate")]
+[string]$CertStoreLocation = "Cert:\CurrentUser\My",
+
+[parameter(Mandatory=$false, ParameterSetName="GenerateCertificate")]
+[DateTime]$NotBefore = [System.DateTime]::Now.Date,
+
+[parameter(Mandatory=$false, ParameterSetName="GenerateCertificate")]
+[DateTime]$NotAfter = $NotBefore.AddYears(1),
+
+[parameter(Mandatory=$false, ParameterSetName="GenerateCertificate")]
+[ValidateScript({if ($_){ Test-Path $_ -PathType Container }})][string]$OutputPath,
+
+[parameter(Mandatory=$true, ParameterSetName="CreateKeyVault")]
+[string]$ResourceGroupName,
+
+[parameter(Mandatory=$true, ParameterSetName="CreateKeyVault")]
+[string]$Location,
+
+[parameter(Mandatory=$true, ParameterSetName="CreateKeyVault")]
+[string]$ApplicationId,
+
+[parameter(Mandatory=$true, ParameterSetName="CreateKeyVault")]
+[parameter(Mandatory=$true, ParameterSetName="SetValues")]
+[string]$KeyVaultName,
+
+[parameter(Mandatory=$false, ParameterSetName="SetValues")]
+[PSObject[]]$Values
 )
 
 function Validate-ValueParameter {
@@ -42,7 +67,34 @@ param(
 
 switch($PSCmdlet.ParameterSetName)
 {
-    "SetupKeyVault"
+    "GenerateCertificate"
+    {
+        # There is an existing bug with PowerShell where $PSScriptRoot is not set if used as a parameter's default value.  We'll work around
+        # this here by checking to see if a value was provided for $OutputPath, and if not, use the default
+        if ($PSBoundParameters.ContainsKey('OutputPath'))
+        {
+            $outputFilePath = Join-Path $OutputPath "$Subject.cer"
+        }
+        else
+        {
+            $OutputFilePath = "$PSScriptRoot\$Subject.cer"
+        }
+
+        $x509Cert = New-SelfSignedCertificate -NotBefore $NotBefore -NotAfter $NotAfter -Subject $Subject -CertStoreLocation $CertStoreLocation -Provider "Microsoft Strong Cryptographic Provider"
+
+        # Export the public key portion of the certificate
+        $cerFileInfo = Export-Certificate -Cert $x509Cert -FilePath $outputFilePath
+        $publicKeyCert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
+        $publicKeyCert.Import($outputFilePath)
+
+        $rawCertData = [System.Convert]::ToBase64String($publicKeyCert.GetRawCertData())
+        $thumbprint = [System.Convert]::ToBase64String($publicKeyCert.GetCertHash())
+        $keyId = [System.Guid]::NewGuid().ToString()
+
+        $jsonForManifest = "{ `"type`": `"AsymmetricX509Cert`", `"usage`": `"Verify`", `"keyId`": `"$keyId`", `"customKeyIdentifier`": `"$thumbprint`", `"value`" : `"$rawCertData`" }"
+        $($jsonForManifest)
+    }
+    "CreateKeyVault"
     {
         Login-AzureRmAccount
         New-AzureRmResourceGroup –Name $ResourceGroupName –Location $Location -ErrorAction Stop
